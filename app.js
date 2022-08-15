e47dbf2c0f045796ef3244e18fd0273bda49b7ee
“Uzair <Uzairmalik2329@gmail.com>
2022-08-16 00:59:09 +0500

    initial

---
+++ app.js
@@ -0,0 +1,535 @@
+// Chaptaer 1(a)
+alert("Thanks for your input!");
+// Chapter 1(b)
+alert("Thanks for your input!");
+// Chapter 2(a)
+var name = "Mark";
+// Chapter 2(b)
+name = "Ace";
+// Chapter 2(c)
+var nationality;
+nationality = "U.S.";
+// Chapter 2(d)
+var floogle = "Mark";
+// Chapter 2(d)
+var lessonAuthor = "Mark";
+var guyWhoKeepsSayingHisOwnName = "Mark";
+var x = "Mark";
+// Chapter 2(e)
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";
+// Chapter 2(f)
+var nickname = "Bub";
+// Chapter 2(g)
+var thanx = "Thanks for your input!"
+alert(thanx);
+// Chapter 3(a)
+var weight = 150;
+// Chapter 3(b)
+var originalNum = 23;
+var newNum = originalNum + 7;
+// Chapter 3(c)
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+// Chapter 3(d)
+var originalNum = 90;
+originalNum = originalNum + 10;
+// Chapter 3(e)
+var originalNum = "23";
+var newNum = originalNum + 7;
+// Chapter 3(f)
+var originalNumber = 23;
+var newNumber = originalNumber + 7;
+// Chapter 3 (g)
+// var 1stPresident = "Washington";
+// Chapter 3(h)
+alert(144);
+// Chapter 3(i)
+var caseQty = 144;
+alert(caseQty);
+// Chapter 5(a)
+var popularNumber = 4;
+// Chapter 5(b)
+var popularNumber = 2 + 2;
+// Chapter 5(c)
+alert(2 + 2);
+// Chapter 5(d)
+var popularNumber = 12 - 24;
+// Chapter 5(e)
+var popularNumber = 3 * 12;
+// Chapter 5(f)
+var num = 10;
+var popularNumber = num + 200;
+// Chapter 5(g)
+var num = 10;
+var anotherNum = 1;
+var popularNumber = num + anotherNum;
+// Chapter 5(h)
+var whatsLeftOver = 10 % 3;
+// Chapter 5(i)
+var whatsLeftOver = 9 % 3;
+// Chapter 6(a)
+var num = 1;
+var newNum = num++;
+// Chapter 6(b)
+var num = 1;
+var newNum = ++num;
+// Chapter 6(c)
+var num = 1;
+var newNum = num--;
+// Chapter 6(d)
+var num = 1;
+var newNum = --num;
+// Chapter 7(a)
+var totalCost = 1 + 3 * 4;
+// Chapter 7(b)
+var totalCost = 1 + (3 * 4);
+// Chapter 7(c)
+var totalCost = (1 + 3) * 4;
+// Chapter 7(d)
+var resultOfComputation = (2 * 4) * 4 + 2;
+// Chapter 7(e)
+resultOfComputation = ((2 * 4) * 4) + 2;
+// Chapter 8(a)
+var message = "Thanks for your input!";
+alert(message);
+// Chapter 8(b)
+alert("Thanks, " + userName + "!");
+// Chapter 8(c)
+var message = "Thanks, ";
+var banger = "!";
+alert(message + userName + banger);
+// Chapter 8(d)
+alert("Thanks, " + "Susan" + "!");
+// Chapter 8(e)
+var message = "Thanks, ";
+var userName = "Susan";
+var banger = "!";
+var customMess = message + userName + banger;
+alert(customMess);
+// Chapter 8(f)
+alert("2" + "2");
+// Chapter 8(g)
+alert("2 plus 2 equals " + 2 + 2);
+// Chapter 9(a)
+var spec = prompt("Your species?", "human");
+// Chapter 9(b)
+var Chapter = "Your species?";
+var defaultAnswer = "human";
+var spec = prompt(Chapter, defaultAnswer);
+// Chapter 9(c)
+var numberOfCats = prompt("How many cats?");
+var tooManyCats = numberOfCats + 1;
+// Chapter 10(a)
+var x = prompt("Where does the Pope live?");
+if (x === "Vatican") {
+    alert("Correct!");
+}
+// Chapter 10(b)
+var correctAnswer = "Vatican";
+if (x === correctAnswer) {
+    alert("Correct!");
+}
+// Chapter 10(c)
+var correctAnswer = "Vatican";
+if (x === correctAnswer) {
+    score++;
+    userIQ = "genius";
+    alert("Correct!");
+}
+// Chapter 11(a)
+if (fullName === "Mark" + " " + "Myers") { }
+if (fullName === firstName + " " + "Myers") { }
+if (fullName === firstName + " " + "Myers") { }
+if (totalCost === 81.50 + 135) { }
+if (totalCost === materialsCost + 135) { }
+if (totalCost === materialsCost + laborCost) { }
+
+// Chapter 11(b)
+if (yourTicketNumber !== 487208) {
+    alert("Better luck next time.");
+}
+// Chapter 11(c)
+if (1 > 0) { }
+if (0 < 1) { }
+if (1 >= 0) { }
+// Chapter 12(a)
+var x = prompt("Where does the Pope live?");
+if (x === "Vatican") {
+    alert("Correct!");
+}
+// Chapter 12(b)
+var x = prompt("Where does the Pope live?");
+if (x === "Vatican") {
+    alert("Correct!");
+}
+if (x !== "Vatican") {
+    alert("Wrong answer");
+}
+// Chapter 12(c)
+if (x === "Vatican") {
+    alert("Correct!");
+}
+else {
+    alert("Wrong answer");
+}
+// Chapter 12(d)
+var correctAnswer = "Vatican";
+if (x === correctAnswer) {
+    alert("Correct!");
+}
+else {
+    score--;
+    userIQ = "problematic";
+    alert("Incorrect");
+}
+// Chapter 12(e)
+var correctAnswer = "Vatican";
+if (x === correctAnswer) {
+    alert("Correct!");
+}
+else if (x === "Rome") {
+    alert("Incorrect but close");
+}
+else {
+    alert("Incorrect");
+}
+// Chapter 13(a)
+if (weight > 300 && time < 6) {
+    alert("Come to our tryout!");
+}
+else {
+    alert("Come to our cookout!");
+}
+// Chapter 13(b)
+if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+    alert("Come to our tryout!");
+}
+else {
+    alert("Come to our cookout!");
+}
+// Chapter 13(c)
+if (SAT > avg || GPA > 2.5 || sport === "football") {
+    alert("Welcome to Bubba State!");
+}
+else {
+    alert("Have you looked into appliance repair?");
+}
+// Chapter 14(a)
+if ((x === y || a === b) && c === d) {
+    g = h;
+}
+else {
+    e = f;
+}
+// Chapter 14(b)
+if (c === d) {
+    if (x === y) {
+        g = h;
+    }
+    else if (a === b) {
+        g = h;
+    }
+    else {
+        e = f;
+    }
+}
+else {
+    e = f;
+}
+// Chapter 15(a)
+var cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles", "Seattle"];
+alert("Welcome to " + cities[3]);
+// Chapter 15(b)
+var mixedArray = [1, "Bob", "Now is", true];
+// Chapter 16(a)
+var pets = [];
+pets[0] = "dog";
+pets[1] = "cat";
+pets[2] = "bird";
+// Chapter 16(b)
+pets[3] = "lizard";
+pets[6] = "snake";
+// Chapter 16(c)
+pets[3] = "lizard";
+pets[4] = "fish";
+pets[5] = "gerbil";
+pets[6] = "snake";
+// Chapter 16(d)
+pets.pop();
+// Chapter 16(e)
+pets.push("fish", "ferret");
+// chapter 17(a)
+pets.shift();
+// Chapter 17(b)
+pets.unshift("fish", "ferret");
+// Chapter 17(c)
+pets.splice(2, 2, "pig", "duck", "emu");
+// Chapter 17(d)
+pets.splice(2, 0, "pig", "duck", "emu");
+// chapter 17(e)
+pets.splice(2, 2);
+// Chapter 17(f)
+var noPets = pets.slice(2, 4);
+// Chapter 18(a)
+var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+if (cityToCheck === cleanestCities[0]) {
+    alert("It's one of the cleanest cities");
+}
+else if (cityToCheck === cleanestCities[1]) {
+    alert("It's one of the cleanest cities");
+}
+else if (cityToCheck === cleanestCities[2]) {
+    alert("It's one of the cleanest cities");
+}
+else if (cityToCheck === cleanestCities[3]) {
+    alert("It's one of the cleanest cities");
+}
+else if (cityToCheck === cleanestCities[4]) {
+    alert("It's one of the cleanest cities");
+}
+else {
+    alert("It's not on the list");
+}
+// Chapter 18(b)
+for (var i = 0; i <= 4; i++) {
+    if (cityToCheck === cleanestCities[i]) {
+        alert("It's one of the cleanest cities");
+    }
+}
+// Chapter 19(a)
+var matchFound = "no";
+for (var i = 0; i <= 4; i++);
+if (cityToCheck === cleanestCities[i]) {
+    matchFound = "yes";
+    alert("It's one of the cleanest cities");
+}
+if (matchFound === "no") {
+    alert("It's not on the list");
+}
+// Chapter 19(b)
+var matchFound = false;
+for (var i = 0; i <= 4; i++);
+if (cityToCheck === cleanestCities[i]) {
+    matchFound = true;
+    alert("It's one of the cleanest cities");
+}
+
+if (matchFound === false) {
+    alert("It's not on the list");
+}
+// Chapter 19(c)
+var matchFound = false;
+for (var i = 0; i <= 4; i++) {
+    ;
+    if (cityToCheck === cleanestCities[i]) {
+        matchFound = true;
+        alert("It's one of the cleanest cities");
+        break;
+    }
+}
+if (matchFound === false) {
+    alert("It's not on the list");
+}
+// Chapter 19(d)
+var numElements = cleanestCities.length;
+var matchFound = false;
+for (var i = 0; i < numElements; i++) {
+    ;
+    if (cityToCheck === cleanestCities[i]) {
+        matchFound = true;
+        alert("It's one of the cleanest cities");
+        break;
+    }
+}
+if (matchFound === false) {
+    alert("It's not on the list");
+}
+// Chapter 20(a)
+var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+var fullNames = [];
+for (var i = 0; i < firstNames.length; i++) {
+    for (var j = 0; j < lastNames.length; j++) {
+        fullNames.push(firstNames[i] + lastNames[j]);
+    }
+}
+// Chapter 21(a)
+var cleanestCities = ["Cheyenne", "cheyenne", "Santa Fe", "santa fe", "Tucson", "tucson", "Great Falls",
+    "great falls", "Honolulu", "honolulu"];
+var cityToCheck = prompt("Enter your city");
+cityToCheck = cityToCheck.toLowerCase();
+var cleanestCities = ["cheyenne", "santa fe", "tucson", "great falls", "honolulu"];
+for (var i = 0; i <= 4; i++) {
+    if (cityToCheck === cleanestCities[i]) {
+        alert("It's one of the cleanest cities");
+    }
+}
+// Chapter 22(a)
+var firstChar = cityToCheck.slice(0, 1);
+// Chapter 22(b)
+var someChars = cityToCheck.slice(2, 5);
+// Chapter 22(c)
+var someChars = cityToCheck.slice(2);
+// Chapter 22(d)
+var firstChar = cityToCheck.slice(0, 1);
+var otherChars = cityToCheck.slice(1);
+firstChar = firstChar.toUpperCase();
+otherChars = otherChars.toLowerCase();
+var cappedCity = firstChar + otherChars;
+// Chapter 22(e)
+var month = prompt("Enter a month");
+var charsInMonth = month.length;
+if (charsInMonth > 3) {
+    monthAbbrev = month.slice(0, 3);
+}
+// Chapter 23(a)
+for (var i = 0; i < text.length; i++) {
+    if (text.slice(i, i + 12) === "World War II") {
+        text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+    }
+}
+var firstChar = text.indexOf("World War II");
+// Chapter 24(a)
+var firstChar = firstName.charAt(0)
+// Chapter 24(b)
+var lastChar = name.charAt(name.length - 1);
+// Chapter 24(c)
+for (var i = 0; i < text.length; i++) {
+    if (text.charAt(i) === "!") {
+        alert("Exclamation point found!");
+        break;
+    }
+}
+// Chapter 25(a)
+for (var i = 0; i < text.length; i++) {
+    if (text.slice(i, i + 12) === "World War II") {
+        text = text.slice(0, 1) + "the Second World War" + text.slice(i + 12);
+    }
+}
+// Chapter 25(b)
+var firstChar = text.indexOf("World War II");
+if (firstChar !== -1) {
+    text = text.slice(0, firstChar) + "the Second World War" + text.slice(firstChar + 12);
+}
+// Chapter 25(c)
+text = text.replace("World War II", "the Second World War");
+// Chapter 25(d)
+var newText = text.replace(/World War II/g, "the Second World War");
+// Chapter 26(a)
+var numberOfStars = Math.round(scoreAvg);
+// Chapter 26(b)
+var scoreAvg = Math.round(.0678437);
+// Chapter 26(b)
+var scoreAvg = Math.ceil(.000001);
+// Chapter 26(c)
+var scoreAvg = Math.floor(.999999);
+// Chapter 27(a)
+var randomNumber = Math.random();
+// Chapter 27(b)
+var bigDecimal = Math.random();
+var improvedNum = (bigDecimal * 6) + 1;
+var numberOfStars = Math.floor(improvedNum);
+// Chapter 28(a)
+var currentAge = prompt("Enter your age.");
+var yearsEligibleToVote = currentAge - 18;
+var profit = "200" - "150";
+// Chapter 28(b)
+var result = "200" + 150;
+var profit = "200" - "duck";
+// Chapter 28(c)
+var currentAge = prompt("Enter your age.");
+var qualifyingAge = currentAge + 1;
+// Chapter 28(d)
+var currentAge = prompt("Enter your age.");
+var qualifyingAge = parseInt(currentAge) + 1;
+// Chapter 28(e)
+var myInteger = parseInt("1.9999");
+// Chapter 28(f)
+var myFractional = parseFloat("1.9999");
+// Chapter 29(a)
+var integerString = "24"
+var num = Number(integerString);
+//  Chapter 29(b)
+var floatingNumString = "24.9876";
+var num = Number(floatingNumString);
+//  Chapter 29(c)
+var numberAsNumber = 1234;
+var numberAsString = numberAsNumber.toString();
+// Chapter 30(a)
+var total = price + (price * taxRate);
+// Chapter 30(b)
+var prettyTotal = total.toFixed(2);
+// Chapter 30(c)
+var currencyTotal = "$" + prettyTotal;
+// Chapter 30(d)
+var prettyTotal = total.toFixed();
+// Chapter 30(d)
+var str = num.toString();
+if (str.charAt(str.length - 1) === "5") {
+    str = str.slice(0, str.length - 1) + "6";
+}
+num = Number(str);
+prettyNum = num.toFixed(2);
+// Chapter 31(a)
+var rightNow = new Date();
+var dateString = rightNow.toString();
+// Chapter 31(b)
+var rightNow = new Date();
+var theDay = rightNow.getDay();
+// Chapter 31(c)
+var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var now = new Date();
+var theDay = now.getDay();
+var nameOfToday = dayNames[theDay];
+// Chapter 32(a)
+var d = new Date();
+var currentMonth = d.getMonth();
+var dayOfMonth = d.getDate();
+var currYr = d.getFullYear();
+var currentHrs = d.getHours();
+var currMins = d.getMinutes();
+var currSecs = d.getSeconds();
+var millsSince = d.getTime();
+// Chapter 33(a)
+var doomsday = new Date("June 30, 2035");
+// Chapter 33(b)
+var msToday = today.getTime();
+var msDoomsday = doomsday.getTime();
+// Chapter 33(c)
+var msDiff = msDoomsday - msToday;
+// Chapter 33(d)
+dDiff = Math.floor(dDiff);
+// Chapter 33(e)
+var d = new Date("July 21, 1983 13:25:00");
+// Chapter 34(a)
+var d = new Date();
+d.setFullYear(2001);
+// Chapter 34(b)
+var d = new Date();
+d.setDate(15);
+// Chapter 34(c)
+var d = new Date();
+d.setHours(13);
+// Chapter 34(d)
+var d = new Date();
+d.setMinutes(05);
+// Chapter 34(e)
+var d = new Date();
+d.setSeconds(55);
+// Chapter 35(a)
+var now = new Date();
+var theHr = now.getHours();
+var theMin = now.getMinutes();
+alert("time: " + theHr + ":" + theMin);
+// Chapter 35(b)
+function tellTime() {
+    var now = new Date();
+    var theHr = now.getHours();
+    var theMin = now.getMinutes();
+    alert("Current time: " + theHr + ":" + theMin);
+}
+// Chapter 35(c)
+tellTime();
